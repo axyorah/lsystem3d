aa06a72124b86ffbb37f64a4809f5d0e95402e2e
axyorah <elvira.bozileva@gmail.com>
2021-05-10 12:17:47 +0200

    fix(display): updated branch width is now correctly displayed on gui on change (used to show width in absolute units, now shows fraction of branch length)

--- js/gui.js
+++ js/gui.js
@@ -43,16 +43,18 @@ branchLenRng.addEventListener('input', function (evt) {
     const fraction = parseFloat(branchWidRng.value);
     lsys.setBranchLen( parseFloat(val) );
     lsys.setBranchWid( parseFloat(val) * fraction);
-    lsys.draw();
+    //lsys.draw();
+    lsys.updateConfig();
 })
 
 branchWidRng.addEventListener('input', function (evt) {
     // recall: branchWid represents a fraction [0,1] from branchLen
     const fraction = this.value;
     const val = branchLenRng.value * fraction;
-    branchWidLbl.innerText = `${branchWidLbl.innerText.split(':')[0]}: ${val}`;
+    branchWidLbl.innerText = `${branchWidLbl.innerText.split(':')[0]}: ${fraction}`;
     lsys.setBranchWid( parseFloat(val) );
-    lsys.draw();
+    //lsys.draw();
+    lsys.updateConfig();
 })
 
 branchColor.addEventListener('change', function (evt) {
@@ -64,21 +66,24 @@ yawRng.addEventListener('input', function (evt) {
     const val = this.value;
     yawLbl.innerText = `${yawLbl.innerText.split(':')[0]}: ${val}`;
     lsys.setAngleYaw( parseFloat(val) );
-    lsys.draw();
+    //lsys.draw();
+    lsys.updateConfig();
 })
 
 pitchRng.addEventListener('input', function (evt) {
     const val = this.value;
     pitchLbl.innerText = `${pitchLbl.innerText.split(':')[0]}: ${val}`;
     lsys.setAnglePitch( parseFloat(val) );
-    lsys.draw();
+    //lsys.draw();
+    lsys.updateConfig();
 })
 
 rollRng.addEventListener('input', function (evt) {
     const val = this.value;
     rollLbl.innerText = `${rollLbl.innerText.split(':')[0]}: ${val}`;
     lsys.setAngleRoll( parseFloat(val) );
-    lsys.draw();
+    //lsys.draw();
+    lsys.updateConfig();
 })
 
 ruleXInpt.addEventListener('change', function (evt) {
