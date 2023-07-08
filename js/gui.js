c1b57e0e6cc1a620e55bbeda44a24e1eff121245
axyorah <elvira.bozileva@gmail.com>
2023-07-08 15:53:14 +0200

    gui now also has a separate class

--- js/gui.js
+++ js/gui.js
@@ -32,163 +32,215 @@ const ruleLInpt = document.querySelector('#rule-L');
 const rulePopup = document.querySelector('#rule-popup');
 const rulePopupLbl = document.querySelector('#rule-popup-lbl');
 
-undoBtn.addEventListener('click', function (evt) {
-    lsys.prev();
-    lsys.build();
-});
+export class GUI {
+    constructor(lsys) {
+        this.lsys = lsys;
 
-restartBtn.addEventListener('click', function (evt) {
-    lsys.reset();
-    lsys.build();
-});
+        this.init.bind(this);
+        this.connect.bind(this);
 
-updateBtn.addEventListener('click', function (evt) {
-    lsys.next();
-    lsys.build();
-});
+        this.next.bind(this);
+        this.prev.bind(this);
+        this.reset.bind(this);
+        this.updateBranchLength.bind(this);
+        this.updateBranchWidth.bind(this);
+        this.updateBranchRatio.bind(this);
+        this.updateBranchColor.bind(this);
+        this.updateLeafLength.bind(this);
+        this.updateLeafWidth.bind(this);
+        this.updateLeafDepth.bind(this);
+        this.updateLeafColor.bind(this);
+        this.updateYaw.bind(this);
+        this.updatePitch.bind(this);
+        this.updateRoll.bind(this);
+        this.updateRuleX.bind(this);
+        this.updateRuleF.bind(this);
+        this.updateRuleL.bind(this);
+    }
 
-branchLenRng.addEventListener('input', function (evt) {
-    // recall: branchWid represents a fraction [0,1] from branchLen
-    const val = this.value;
-    branchLenLbl.innerText = `${branchLenLbl.innerText.split(':')[0]}: ${val}`;
-    // update both branch len and width
-    const fraction = parseFloat(branchWidRng.value);
-    lsys.map['F'].len = parseFloat(val);
-    lsys.map['F'].wid = parseFloat(val) * fraction;
-    lsys.update();
-});
+    init() {
+        // default settings
+        const branchLen = parseFloat(branchLenRng.value);
+        const branchWid = parseFloat(branchWidRng.value);
+        const branchRat = parseFloat(branchRatioRng.value);
+        const branchCol = branchColor.value;
+        const leafLen = parseFloat(leafLenRng.value);
+        const leafWid = parseFloat(leafWidRng.value);
+        const leafDep = parseFloat(leafDepRng.value);
+        const leafCol = leafColor.value;
+        const yaw = parseFloat(yawRng.value);
+        const pitch = parseFloat(pitchRng.value);
+        const roll = parseFloat(rollRng.value);
 
-branchWidRng.addEventListener('input', function (evt) {
-    // recall: branchWid represents a fraction [0,1] from branchLen
-    const fraction = parseFloat(this.value);
-    const val = parseFloat(branchLenRng.value) * fraction;
-    branchWidLbl.innerText = `${
-        branchWidLbl.innerText.split(':')[0]
-    }: ${fraction}`;
-    lsys.map['F'].wid = parseFloat(val);
-    lsys.update();
-});
+        this.lsys.map['F'].wid = branchWid;
+        this.lsys.map['F'].len = branchLen;
+        this.lsys.map['F'].ratio = branchRat;
+        this.lsys.map['F'].color = branchCol;
+        this.lsys.map['L'].wid = leafWid;
+        this.lsys.map['L'].len = leafLen;
+        this.lsys.map['L'].dep = leafDep;
+        this.lsys.map['L'].color = leafCol;
+        this.lsys.yaw = yaw;
+        this.lsys.pitch = pitch;
+        this.lsys.roll = roll;
+    }
 
-branchRatioRng.addEventListener('input', function (evt) {
-    const val = this.value;
-    branchRatioLbl.innerText = `${
-        branchRatioLbl.innerText.split(':')[0]
-    }: ${val}`;
-    lsys.map['F'].ratio = parseFloat(val);
-    lsys.update();
-});
+    next() {
+        this.lsys.next();
+        this.lsys.build();
+    }
 
-branchColor.addEventListener('change', function (evt) {
-    const val = this.value;
-    lsys.map['F'].color = val;
-    lsys.update();
-});
+    prev() {
+        this.lsys.prev();
+        this.lsys.build();
+    }
 
-leafLenRng.addEventListener('input', function (evt) {
-    // recall: branchWid represents a fraction [0,1] from branchLen
-    const val = this.value;
-    leafLenLbl.innerText = `${leafLenLbl.innerText.split(':')[0]}: ${val}`;
-    lsys.map['L'].len = parseFloat(val);
-    lsys.update();
-});
+    reset() {
+        this.lsys.reset();
+        this.lsys.build();
+    }
 
-leafWidRng.addEventListener('input', function (evt) {
-    // recall: branchWid represents a fraction [0,1] from branchLen
-    const val = this.value;
-    leafWidLbl.innerText = `${leafWidLbl.innerText.split(':')[0]}: ${val}`;
-    lsys.map['L'].wid = parseFloat(val);
-    lsys.update();
-});
+    updateBranchLength() {
+        // recall: branchWid represents a fraction [0,1] from branchLen
+        const val = branchLenRng.value;
+        const label = branchLenLbl.innerText.split(':')[0];
+        branchLenLbl.innerText = `${label}: ${val}`;
+        // update both branch len and width
+        const fraction = parseFloat(branchWidRng.value);
+        this.lsys.map['F'].len = parseFloat(val);
+        this.lsys.map['F'].wid = parseFloat(val) * fraction;
+        this.lsys.update();
+    }
 
-leafDepRng.addEventListener('input', function (evt) {
-    // recall: branchWid represents a fraction [0,1] from branchLen
-    const val = this.value;
-    leafDepLbl.innerText = `${leafDepLbl.innerText.split(':')[0]}: ${val}`;
-    lsys.map['L'].dep = parseFloat(val);
-    lsys.update();
-});
+    updateBranchWidth() {
+        // recall: branchWid represents a fraction [0,1] from branchLen
+        const fraction = parseFloat(branchWidRng.value);
+        const val = parseFloat(branchLenRng.value) * fraction;
+        const label = branchWidLbl.innerText.split(':')[0];
+        branchWidLbl.innerText = `${label}: ${fraction}`;
+        this.lsys.map['F'].wid = parseFloat(val);
+        this.lsys.update();
+    }
 
-leafColor.addEventListener('change', function (evt) {
-    const val = this.value;
-    lsys.map['L'].color = val;
-    lsys.update();
-});
+    updateBranchRatio() {
+        const val = branchRatioRng.value;
+        const label = branchRatioLbl.innerText.split(':')[0];
+        branchRatioLbl.innerText = `${label}: ${val}`;
+        this.lsys.map['F'].ratio = parseFloat(val);
+        this.lsys.update();
+    }
 
-yawRng.addEventListener('input', function (evt) {
-    const val = this.value;
-    yawLbl.innerText = `${yawLbl.innerText.split(':')[0]}: ${val}`;
-    lsys.yaw = parseFloat(val);
-    lsys.update();
-});
+    updateBranchColor() {
+        const val = branchColor.value;
+        this.lsys.map['F'].color = val;
+        this.lsys.update();
+    }
 
-pitchRng.addEventListener('input', function (evt) {
-    const val = this.value;
-    pitchLbl.innerText = `${pitchLbl.innerText.split(':')[0]}: ${val}`;
-    lsys.pitch = parseFloat(val);
-    lsys.update();
-});
+    updateLeafLength() {
+        // recall: branchWid represents a fraction [0,1] from branchLen
+        const val = leafLenRng.value;
+        leafLenLbl.innerText = `${leafLenLbl.innerText.split(':')[0]}: ${val}`;
+        this.lsys.map['L'].len = parseFloat(val);
+        this.lsys.update();
+    }
 
-rollRng.addEventListener('input', function (evt) {
-    const val = this.value;
-    rollLbl.innerText = `${rollLbl.innerText.split(':')[0]}: ${val}`;
-    lsys.roll = parseFloat(val);
-    lsys.update();
-});
+    updateLeafWidth() {
+        // recall: branchWid represents a fraction [0,1] from branchLen
+        const val = leafWidRng.value;
+        leafWidLbl.innerText = `${leafWidLbl.innerText.split(':')[0]}: ${val}`;
+        this.lsys.map['L'].wid = parseFloat(val);
+        this.lsys.update();
+    }
 
-ruleXInpt.addEventListener('change', function (evt) {
-    const val = this.value;
-    lsys.rules = { X: val };
+    updateLeafDepth() {
+        // recall: branchWid represents a fraction [0,1] from branchLen
+        const val = leafDepRng.value;
+        leafDepLbl.innerText = `${leafDepLbl.innerText.split(':')[0]}: ${val}`;
+        this.lsys.map['L'].dep = parseFloat(val);
+        this.lsys.update();
+    }
 
-    lsys.prev();
-    lsys.next();
-    lsys.build();
-});
+    updateLeafColor() {
+        const val = leafColor.value;
+        this.lsys.map['L'].color = val;
+        this.lsys.update();
+    }
 
-ruleFInpt.addEventListener('change', function (evt) {
-    const val = this.value;
-    lsys.rules = { F: val };
+    updateYaw() {
+        const val = yawRng.value;
+        yawLbl.innerText = `${yawLbl.innerText.split(':')[0]}: ${val}`;
+        this.lsys.yaw = parseFloat(val);
+        this.lsys.update();
+    }
 
-    lsys.prev();
-    lsys.next();
-    lsys.build();
-});
+    updatePitch() {
+        const val = pitchRng.value;
+        pitchLbl.innerText = `${pitchLbl.innerText.split(':')[0]}: ${val}`;
+        this.lsys.pitch = parseFloat(val);
+        this.lsys.update();
+    }
 
-ruleLInpt.addEventListener('change', function (evt) {
-    const val = this.value;
-    lsys.rules = { L: val };
+    updateRoll() {
+        const val = rollRng.value;
+        rollLbl.innerText = `${rollLbl.innerText.split(':')[0]}: ${val}`;
+        this.lsys.roll = parseFloat(val);
+        this.lsys.update();
+    }
 
-    lsys.prev();
-    lsys.next();
-    lsys.build();
-});
+    updateRuleX() {
+        const val = ruleXInpt.value;
+        this.lsys.rules = { X: val };
 
-rulePopupLbl.addEventListener('click', function (evt) {
-    rulePopup.classList.toggle('show');
-});
+        this.lsys.prev();
+        this.lsys.next();
+        this.lsys.build();
+    }
 
-window.addEventListener('load', (evt) => {
-    // default settings
-    const branchLen = parseFloat(branchLenRng.value);
-    const branchWid = parseFloat(branchWidRng.value);
-    const branchRat = parseFloat(branchRatioRng.value);
-    const branchCol = branchColor.value;
-    const leafLen = parseFloat(leafLenRng.value);
-    const leafWid = parseFloat(leafWidRng.value);
-    const leafDep = parseFloat(leafDepRng.value);
-    const leafCol = leafColor.value;
-    const yaw = parseFloat(yawRng.value);
-    const pitch = parseFloat(pitchRng.value);
-    const roll = parseFloat(rollRng.value);
+    updateRuleF() {
+        const val = ruleFInpt.value;
+        this.lsys.rules = { F: val };
 
-    lsys.map['F'].wid = branchWid;
-    lsys.map['F'].len = branchLen;
-    lsys.map['F'].ratio = branchRat;
-    lsys.map['F'].color = branchCol;
-    lsys.map['L'].wid = leafWid;
-    lsys.map['L'].len = leafLen;
-    lsys.map['L'].dep = leafDep;
-    lsys.map['L'].color = leafCol;
-    lsys.yaw = yaw;
-    lsys.pitch = pitch;
-    lsys.roll = roll;
-});
+        this.lsys.prev();
+        this.lsys.next();
+        this.lsys.build();
+    }
+
+    updateRuleL() {
+        const val = ruleLInpt.value;
+        this.lsys.rules = { L: val };
+
+        this.lsys.prev();
+        this.lsys.next();
+        this.lsys.build();
+    }
+
+    connect() {
+        updateBtn.addEventListener('click', this.next);
+        restartBtn.addEventListener('click', this.reset);
+        undoBtn.addEventListener('click', this.prev);
+
+        branchLenRng.addEventListener('input', this.updateBranchLength);
+        branchWidRng.addEventListener('input', this.updateBranchWidth);
+        branchRatioRng.addEventListener('input', this.updateBranchRatio);
+        branchColor.addEventListener('change', this.updateBranchColor);
+
+        leafLenRng.addEventListener('input', this.updateLeafLength);
+        leafWidRng.addEventListener('input', this.updateLeafWidth);
+        leafDepRng.addEventListener('input', this.updateLeafDepth);
+        leafColor.addEventListener('change', this.updateLeafColor);
+
+        yawRng.addEventListener('input', this.updateYaw);
+        pitchRng.addEventListener('input', this.updatePitch);
+        rollRng.addEventListener('input', this.updateRoll);
+
+        ruleXInpt.addEventListener('change', this.updateRuleX);
+        ruleFInpt.addEventListener('change', this.updateRuleF);
+        ruleLInpt.addEventListener('change', this.updateRuleL);
+
+        rulePopupLbl.addEventListener('click', function () {
+            rulePopup.classList.toggle('show');
+        });
+
+        window.addEventListener('load', this.init);
+    }
+}
